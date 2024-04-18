3a4cc8817d267cc1d453a6f353ea5894ac516497
unknown <sebastian.lopezd@udea.edu.co>
2024-04-18 12:53:09 -0500

    generando señales

--- Evidencia/GeneracionPutos/tringular.m
+++ Evidencia/GeneracionPutos/tringular.m
@@ -4,8 +4,8 @@ clc
 % Definir el dominio de la señal desde -pi a pi
 t = linspace(0, 4*pi, 500);
 
-% Generar la señal senoidal
-senal = sin(t);
+% Generar la señal triangular
+senal = sawtooth(t,0.5); % Amplitud predeterminada y periodo 2pi
 
 % Escalar la señal al rango de 0 a 255
 senal_escalada = (senal + 1) * 127.5;
@@ -19,11 +19,7 @@ senal_redondeada(senal_redondeada > 255) = 255;
 
 % Graficar la señal generada
 plot(t, senal_redondeada);
-title('Señal senoidal de -pi a pi con resolución de 8 bits');
+title('Señal triangular de 0 a 4*pi con resolución de 8 bits');
 xlabel('Tiempo (rad)');
 ylabel('Valor');
 grid on;
-
-% Convertir valores redondeados a binario
-%senal_binaria = dec2bin(senal_redondeada);
-%senal_hexadecimal = dec2hex(senal_redondeada);
